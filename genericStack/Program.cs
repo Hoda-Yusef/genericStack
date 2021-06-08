1e161860d924fced52e65e04ff87ea2fca17518c
Yusef Aborokon <yusefaborokon@gmail.com>
2021-06-08 13:58:57 +0300

    Add project files.

---
+++ genericStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericStack
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            specialStack<double> s = new specialStack<double>(10);
+            int i;
+            Random rnd = new Random();
+
+            for(i=0;i<10;i++)
+            {
+                s.push(rnd.NextDouble() * 10);
+            }
+
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
---
+++ genericStack/specialStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericStack
+{
+    class specialStack
+    {
+        private T[] arr;
+        private int count;
+
+        public specialStack(int size)
+        {
+            arr = new T[size];
+            count = 0;
+        }
+
+        public void push(Task elment)
+        {
+            if (count < arr.Length)
+            {
+                arr[count] = elment;
+                count++;
+            }
+
+        }
+
+        public T pop()
+        {
+            if (count > 0)
+            {
+                return arr[--count];
+            }
+            else
+                return default(T);
+        }
+
+        public T Top()
+        {
+            if (count > 0)
+            {
+                return arr[count-1];
+            }
+            else
+                return default(T);
+        }
+
+        public bool isEmpty()
+        {
+            return (count == 0);
+        }
+
+        public override string ToString()
+        {
+            string str = string.Empty;
+            for(int i=0;i<count;i++)
+            {
+                str += Environment.NewLine + arr[i];
+            }
+            return str;
+        }
+    }
+}
